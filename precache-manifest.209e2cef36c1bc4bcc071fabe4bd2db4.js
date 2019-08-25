1996bbb79fd343b7cc3c5950ec4e8203b027fe4f
Jalfaro91 <jpalfarogodoy@gmail.com>
2019-08-25 18:12:11 -0400

    Updates

---
+++ precache-manifest.209e2cef36c1bc4bcc071fabe4bd2db4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec6c9828d565d2630ed2b9ef18714b5",
+    "url": "/Jalfaro91/sachiT/index.html"
+  },
+  {
+    "revision": "f70aad964dd9ec759473",
+    "url": "/Jalfaro91/sachiT/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3da6dac19a9505ac76a7",
+    "url": "/Jalfaro91/sachiT/static/js/2.647ab03f.chunk.js"
+  },
+  {
+    "revision": "f70aad964dd9ec759473",
+    "url": "/Jalfaro91/sachiT/static/js/main.e81250e7.chunk.js"
+  },
+  {
+    "revision": "50b5abf2426643d86993",
+    "url": "/Jalfaro91/sachiT/static/js/runtime~main.da3bd7c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Jalfaro91/sachiT/static/media/logo.5d5d9eef.svg"
+  }
+]);
