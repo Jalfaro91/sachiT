69d471fc44b5b3bf0843b9a500a02ccba9017a3e
Jalfaro91 <jpalfarogodoy@gmail.com>
2019-08-25 18:59:53 -0400

    ADDED: sachiBG & Logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import logo from './img/logo_transparent.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-img">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Sachit
-        </p>
-        <a
+        {/* <p>
+          Pet Stuffs 
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
